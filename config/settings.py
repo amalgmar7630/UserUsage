589cee4537affedecbf20555899417417a4fd3e9
amalgmar7630 <amalgmar7+1@gmail.com>
2021-03-20 11:18:06 +0100

    add model usage_types

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usage_types.apps.UsageTypesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
